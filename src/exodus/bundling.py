f52ebed96ddc0397dcb3a8478b3b3fc1e3dd2159
Evan Sangaline <esangaline@gmail.com>
2018-01-26 18:13:26 -0500

    Add a method for finding all library dependencies.

--- src/exodus/bundling.py
+++ src/exodus/bundling.py
@@ -4,6 +4,19 @@ from subprocess import PIPE
 from subprocess import Popen
 
 
+def find_all_library_dependencies(binary):
+    """Finds all libraries that a binary directly or indirectly links to."""
+    all_dependencies = set()
+    unprocessed_dependencies = set(find_direct_library_dependencies(binary))
+    while len(unprocessed_dependencies):
+        all_dependencies |= unprocessed_dependencies
+        new_dependencies = set()
+        for dependency in unprocessed_dependencies:
+            new_dependencies |= set(find_direct_library_dependencies(dependency))
+        unprocessed_dependencies = new_dependencies - all_dependencies
+    return list(all_dependencies)
+
+
 def find_direct_library_dependencies(binary):
     """Finds the libraries that a binary directly links to."""
     return [match.group(1) for match in filter(None,
