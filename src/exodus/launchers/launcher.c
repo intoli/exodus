09d248462ce65f82d52383eb5ca60a6fd393c466
Evan Sangaline <esangaline@gmail.com>
2018-01-26 15:27:00 -0500

    Add a launcher written in C.

---
+++ src/exodus/launchers/launcher.c
@@ -0,0 +1,35 @@
+#include <libgen.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main( int argc, char *argv[] )  {
+    char *ld_filename = "{{ld_filename}}";
+    char *executable_filename = "{{executable_filename}}";
+
+    char buffer[2048] = { 0 };
+    if (readlink("/proc/self/exe", buffer, sizeof(buffer) - 4 - strlen(ld_filename) - strlen(executable_filename))) {
+       char *root_directory = dirname(buffer);
+       char library_directory[2048] = { 0 };
+       strcpy(library_directory, root_directory);
+       strcat(library_directory, "/lib/");
+
+       char full_ld_path[2048] = { 0 };
+       strcpy(full_ld_path, library_directory);
+       strcat(full_ld_path, ld_filename);
+
+       char full_executable_path[2048] = { 0 };
+       strcpy(full_executable_path, root_directory);
+       strcat(full_executable_path, "/");
+       strcat(full_executable_path, executable_filename);
+
+       char *ld_args[] = { "--library-path", library_directory, "--inhibit-rpath", "", full_executable_path };
+       char **combined_args = malloc(sizeof(ld_args) + sizeof(char*) * (argc + 1));
+       combined_args[0] = ld_filename;
+       memcpy(combined_args + 1, ld_args, sizeof(ld_args));
+       memcpy(combined_args + (sizeof(ld_args) / sizeof(char*)) + 1, argv + 1, sizeof(char*)*(argc - 1));
+       combined_args[((sizeof(ld_args) + sizeof(char*) * (argc + 1)) / sizeof(char*)) - 1] = NULL;
+       execv(full_ld_path, combined_args);
+    }
+    return 1;
+}
