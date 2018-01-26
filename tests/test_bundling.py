488a3be6d0945c5c2be5502fb31852c8a13d86cf
Evan Sangaline <esangaline@gmail.com>
2018-01-26 17:55:28 -0500

    Add a test for finding the direct binary dependencies.

--- tests/test_bundling.py
+++ tests/test_bundling.py
@@ -1,6 +1,16 @@
+import os
 import sys
 
+from exodus.bundling import find_direct_library_dependencies
 from exodus.bundling import run_ldd
+
+
+def test_find_direct_library_dependencies():
+    dependencies = find_direct_library_dependencies(sys.executable)
+    assert all(dependency.startswith('/') for dependency in dependencies), \
+        'Dependencies should be absolute paths.'
+    assert any('libpython' in line for line in run_ldd(sys.executable)), \
+        '"libpython" was not found as a direct dependency of the python executable.'
 
 
 def test_run_ldd():
