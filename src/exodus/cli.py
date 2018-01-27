d1c3effae110e1a8047dd24a7efb87ea10d24280
Evan Sangaline <esangaline@gmail.com>
2018-01-27 09:04:51 -0500

    Add argument parsing and define the basic CLI API.

--- src/exodus/cli.py
+++ src/exodus/cli.py
@@ -1,5 +1,60 @@
 from __future__ import print_function
 
+import argparse
+
+
+def parse_args(args=None, namespace=None):
+    """Constructs an argument parser and parses the arguments. The default behavior is
+    to parse the arguments from `sys.argv`. A dictionary is returned rather than the
+    typical namespace produced by `argparse`."""
+    formatter = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=formatter, description=(
+        'Bundle ELF binary executables with all of their runtime dependencies '
+        'so that they can be relocated to other systems with incompatible system '
+        'libraries.'
+    ))
+
+    parser.add_argument('executable', metavar='EXECUTABLE', nargs='+', help=(
+        'One or more ELF executables to include in the exodus bundle.'
+    ))
+
+    parser.add_argument('-c', '--chroot', metavar='DIRECTORY', default='/', help=(
+        'A directory that will be treated as the system root while resolving dependencies. '
+        'This is primarily useful for bundling either relocated binaries or ones installed '
+        'in a chroot environment.'
+    ))
+
+    parser.add_argument('-o', '--output', metavar='OUTPUT_FILE',
+        default='./exodus-{{executables}}-bundle.{{extension}}',
+        help=(
+            'The file where the bundle will be written out to. The extension depends on the '
+            'output type. The "{{executables}}" and "{{extension}}" template strings can be '
+            ' used in the provided filename.'
+        ),
+    )
+
+    parser.add_argument('-q', '--quiet', action='store_true', help=(
+        'Suppress warning messages.'
+    ))
+
+    parser.add_argument('-r', '--rename', metavar='NEW_NAME', nargs=1, action='append',
+        default=[], help=(
+            'Renames the binary executable(s) before packaging. The order of rename tags must '
+            'match the order of positional executable arguments.'
+        ),
+    )
+
+    parser.add_argument('-t', '--tarball', action='store_true', help=(
+        'Creates a tarball for manual extraction instead of an installation script. '
+        'Note that this will change the output extension from ".sh" to ".tgz".'
+    ))
+
+    parser.add_argument('-v', '--verbose', action='store_true', help=(
+        'Output additional informational messages.'
+    ))
+
+    return vars(parser.parse_args(args, namespace))
+
 
 def main():
     print('This is where the application goes.')
