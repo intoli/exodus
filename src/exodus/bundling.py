1dbf8c972a7ed94a10099cb4327b39cfbafc0f68
Evan Sangaline <esangaline@gmail.com>
2018-01-27 14:03:04 -0500

    Add a rough pass of creating an unpackaged bundle.

--- src/exodus/bundling.py
+++ src/exodus/bundling.py
@@ -2,16 +2,100 @@ import hashlib
 import logging
 import os
 import re
+import shutil
+import tempfile
 from subprocess import PIPE
 from subprocess import Popen
+
+from exodus.launchers import construct_bash_launcher
 
 
 logger = logging.getLogger(__name__)
 
 
 def create_bundle(**kwargs):
-    logger.error('Not yet implemented.')
-    logger.info('Keys: %s' % kwargs.keys())
+    """Handles the creation of the full bundle."""
+    try:
+        logger.error('Not yet implemented.')
+        root_directory = create_unpackaged_bundle(**kwargs)
+        logger.info('Keys: %s' % kwargs.keys())
+    finally:
+        shutil.rmtree(root_directory)
+
+
+def create_unpackaged_bundle(rename, executables, ldd, **kwargs):
+    """Creates a temporary directory containing the unpackaged contents of the bundle."""
+    root_directory = tempfile.mkdtemp(prefix='exodus-bundle-')
+    try:
+        # Make the top-level bundle directories.
+        bin_directory = os.path.join(root_directory, 'bin')
+        os.makedirs(bin_directory)
+        lib_directory = os.path.join(root_directory, 'lib')
+        os.makedirs(lib_directory)
+        bundles_directory = os.path.join(root_directory, 'bundles')
+
+        # Loop through and package each executable.
+        assert len(executables), 'No executables were specified.'
+        assert len(executables) >= len(rename), \
+            'More renamed options were included than executables.'
+        # Pad the rename's so that they have the same length for the `zip()` call.
+        rename = rename + [None for i in range(len(executables) - len(rename))]
+        for name, executable in zip(rename, executables):
+            # Make the bundle sundirectories for this executable.
+            binary_name = (name or os.path.basename(executable)).replace(os.sep, '')
+            bundle_directory = os.path.join(bundles_directory, binary_name)
+            bundle_bin_directory = os.path.join(bundle_directory, 'bin')
+            os.makedirs(bundle_bin_directory)
+            bundle_lib_directory = os.path.join(bundle_directory, 'lib')
+            os.makedirs(bundle_lib_directory)
+
+            # Copy over the library dependencies and link them.
+            dependencies = find_all_library_dependencies(ldd, executable)
+            for dependency in dependencies:
+                # Create the `lib/{hash}` subdirectory.
+                dependency_name = os.path.basename(dependency)
+                dependency_hash = sha256_hash(dependency)
+                dependency_directory = os.path.join(lib_directory, dependency_hash)
+
+                # Create the actual library file if it doesn't exist.
+                dependency_destination = os.path.join(dependency_directory, 'data')
+                if not os.path.exists(dependency_directory):
+                    os.makedirs(dependency_directory)
+                    shutil.copy(dependency, dependency_destination)
+
+                # Create a link to the actual file using the real library name.
+                dependency_link = os.path.join(dependency_directory, dependency_name)
+                if not os.path.exists(dependency_link):
+                    os.symlink(os.path.join('.', 'data'), dependency_link)
+
+                # Create a secondary link to *that* link from inside the bundle lib directory.
+                bundle_dependency_link = os.path.join(bundle_lib_directory, dependency_name)
+                relative_dependency_path = os.path.relpath(dependency_link, bundle_lib_directory)
+                assert not os.path.exists(bundle_dependency_link), \
+                    'The same library filename has been included more than once in a bundle.'
+                os.symlink(relative_dependency_path, bundle_dependency_link)
+
+            # Copy over the executable.
+            bundle_executable_path = os.path.join(bundle_bin_directory, binary_name)
+            shutil.copy(executable, bundle_executable_path)
+
+            # Construct the launcher.
+            linker_candidates = list(filter(lambda candidate: candidate.startswith('ld-'), (
+                os.path.basename(dependency) for dependency in dependencies
+            )))
+            assert len(linker_candidates) > 0, 'No linker candidates found.'
+            assert len(linker_candidates) < 2, 'Multiple linker candidates found.'
+            [linker] = linker_candidates
+            launcher_path = '%s-launcher.sh' % bundle_executable_path
+            with open(launcher_path, 'w') as f:
+                launcher_content = construct_bash_launcher(linker=linker, binary=binary_name)
+                f.write(launcher_content)
+            shutil.copymode(bundle_executable_path, launcher_path)
+            executable_link = os.path.join(bin_directory, binary_name)
+            relative_launcher_path = os.path.relpath(launcher_path, bin_directory)
+            os.symlink(relative_launcher_path, executable_link)
+    finally:
+        return root_directory
 
 
 def find_all_library_dependencies(ldd, binary):
