1b5d92933a5281ab4acf3cca8a12135eb79aae64
Evan Sangaline <esangaline@gmail.com>
2018-01-27 16:45:18 -0500

    Add tests for the C compilation.

--- tests/test_launchers.py
+++ tests/test_launchers.py
@@ -1,4 +1,19 @@
+import os
+import stat
+import tempfile
+from subprocess import PIPE
+from subprocess import Popen
+
+import pytest
+
+from exodus.launchers import CompilerNotFoundError
+from exodus.launchers import compile_diet
+from exodus.launchers import compile_musl
 from exodus.launchers import construct_bash_launcher
+
+
+parent_directory = os.path.dirname(os.path.realpath(__file__))
+fizz_buzz_source_file = os.path.join(parent_directory, 'data', 'binaries', 'fizz-buzz.c')
 
 
 def test_construct_bash_launcher():
@@ -7,3 +22,29 @@ def test_construct_bash_launcher():
     assert script_content.startswith('#! /bin/bash\n')
     assert linker in script_content
     assert binary in script_content
+
+
+@pytest.mark.parametrize('compiler', ['diet', 'musl'])
+def test_compile(compiler):
+    with open(fizz_buzz_source_file, 'r') as f:
+        code = f.read()
+    compile = compile_diet if compiler == 'diet' else None
+    compile = compile or (compile_musl if compiler == 'musl' else None)
+    try:
+        content = compile(code)
+    except CompilerNotFoundError:
+        # We'll that's a bummer, but better to test these when the are available that
+        # to not test them at all.
+        return
+
+    f, filename = tempfile.mkstemp()
+    os.close(f)
+    with open(filename, 'wb') as f:
+        f.write(content)
+    st = os.stat(filename)
+    os.chmod(f.name, st.st_mode | stat.S_IXUSR)
+
+    process = Popen(f.name, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    assert 'FIZZBUZZ' in stdout.decode('utf-8')
+    assert len(stderr.decode('utf-8')) == 0
