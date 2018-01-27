aefbf15e1566b7d776c8141090a82f9af430d350
Evan Sangaline <esangaline@gmail.com>
2018-01-27 13:01:34 -0500

    Add a test for rendering template files.

--- tests/test_templating.py
+++ tests/test_templating.py
@@ -1,4 +1,11 @@
+import os
+
 from exodus.templating import render_template
+from exodus.templating import render_template_file
+
+
+parent_directory = os.path.dirname(os.path.realpath(__file__))
+data_directory = os.path.join(parent_directory, 'data')
 
 
 def test_render_template():
@@ -6,3 +13,11 @@ def test_render_template():
     expected = 'Hello, my name is Evan.'
     result = render_template(template, greeting='Hello', name='Evan')
     assert expected == result
+
+
+def test_render_template_file():
+    template_file = os.path.join(data_directory, 'template.txt')
+    result = render_template_file(template_file, noun='word', location='here')
+    with open(os.path.join(data_directory, 'template-result.txt'), 'r') as f:
+        expected_result = f.read()
+    assert result == expected_result
