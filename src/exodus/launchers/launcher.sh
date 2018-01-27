4e46cf054c54fc902705286441d91ec68217834e
Evan Sangaline <esangaline@gmail.com>
2018-01-27 13:57:16 -0500

    Use an alternative mechanism to find the current directory of a bash script.
    The previous one was not following symlinks.

--- src/exodus/launchers/launcher.sh
+++ src/exodus/launchers/launcher.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-current_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+current_directory="$(dirname "$(readlink -f "$0")")"
 lib_directory="${current_directory}/../lib/"
 linker="${lib_directory}/{{linker}}"
 executable="${current_directory}/{{binary}}"
