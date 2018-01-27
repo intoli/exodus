c8a0992883f5358fe30e49306aeedb4d504a3370
Evan Sangaline <esangaline@gmail.com>
2018-01-27 10:19:07 -0500

    Change the ldd API to force specifying the ldd to use.

--- src/exodus/bundling.py
+++ src/exodus/bundling.py
@@ -4,29 +4,29 @@ from subprocess import PIPE
 from subprocess import Popen
 
 
-def find_all_library_dependencies(binary):
+def find_all_library_dependencies(ldd, binary):
     """Finds all libraries that a binary directly or indirectly links to."""
     all_dependencies = set()
-    unprocessed_dependencies = set(find_direct_library_dependencies(binary))
+    unprocessed_dependencies = set(find_direct_library_dependencies(ldd, binary))
     while len(unprocessed_dependencies):
         all_dependencies |= unprocessed_dependencies
         new_dependencies = set()
         for dependency in unprocessed_dependencies:
-            new_dependencies |= set(find_direct_library_dependencies(dependency))
+            new_dependencies |= set(find_direct_library_dependencies(ldd, dependency))
         unprocessed_dependencies = new_dependencies - all_dependencies
     return list(all_dependencies)
 
 
-def find_direct_library_dependencies(binary):
+def find_direct_library_dependencies(ldd, binary):
     """Finds the libraries that a binary directly links to."""
-    matches = filter(None, (re.search('=>\s*([^(]*?)\s*\(', line) for line in run_ldd(binary)))
+    matches = filter(None, (re.search('=>\s*([^(]*?)\s*\(', line) for line in run_ldd(ldd, binary)))
     return [match.group(1) for match in matches]
 
 
-def run_ldd(binary):
+def run_ldd(ldd, binary):
     """Runs `ldd` and gets the combined stdout/stderr output as a list of lines."""
     if not os.path.exists(binary):
         raise FileNotFoundError('"%s" is not a file.')
-    process = Popen(['ldd', binary], stdout=PIPE, stderr=PIPE)
+    process = Popen([ldd, binary], stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     return stdout.decode('utf-8').split('\n') + stderr.decode('utf-8').split('\n')
