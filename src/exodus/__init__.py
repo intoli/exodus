4699718274cfb44b6e9b8342b4692de934739d0b
Evan Sangaline <esangaline@gmail.com>
2018-01-27 11:32:07 -0500

    Make the default log handler a NullHandler so there's no output.

--- src/exodus/__init__.py
+++ src/exodus/__init__.py
@@ -4,6 +4,4 @@ import logging
 __version__ = '0.0.1'
 
 root_logger = logging.getLogger(__name__)
-if root_logger.handlers:
-    for handler in root_logger.handlers:
-        root_logger.removeHandler(handler)
+root_logger.handlers = [logging.NullHandler()]
