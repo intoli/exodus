3eb6f0dea6e71a8b3333b0e865f1de1d1c00c585
Evan Sangaline <esangaline@gmail.com>
2018-01-27 09:13:48 -0500

    Add a couple of simple tests for the command line parsing.

---
+++ tests/test_cli.py
@@ -0,0 +1,17 @@
+# from argparse import InvocationError
+import pytest
+
+from exodus.cli import parse_args
+
+
+def test_required_argument():
+    with pytest.raises(SystemExit):
+        parse_args([])
+    parse_args(['/bin/bash'])
+
+
+def test_quiet_and_verbose_flags():
+    result = parse_args(['--quiet', '/bin/bash'])
+    assert result['quiet'] and not result['verbose']
+    result = parse_args(['--verbose', '/bin/bash'])
+    assert result['verbose'] and not result['quiet']
