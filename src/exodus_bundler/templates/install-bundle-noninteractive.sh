a980b2d1de40bb8bc0b9c9971885a63615c7e17f
Evan Sangaline <esangaline@gmail.com>
2018-02-02 12:58:38 -0500

    Fix some minor double quoting issues in the non-interactive install script.

--- src/exodus_bundler/templates/install-bundle-noninteractive.sh
+++ src/exodus_bundler/templates/install-bundle-noninteractive.sh
@@ -2,7 +2,7 @@
 
 user_directory="${HOME}/.exodus"
 output_directory=/opt/exodus
-mkdir -p ${output_directory} 2> /dev/null
+mkdir -p "${output_directory}" 2> /dev/null
 if [ ! $? ] || [ ! -w ${output_directory} ] ; then
     output_directory=${user_directory}
 fi
@@ -15,7 +15,7 @@ base64 -d << "END_OF_FILE" | tar -C "${output_directory}" --strip 1 --no-same-ow
 {{base64_encoded_tarball}}
 END_OF_FILE
 if [ $? -eq 0 ]; then
-    echo "Successfully installed, be sure to add "${output_directory}/bin" to your \$PATH."
+    echo "Successfully installed, be sure to add ${output_directory}/bin to your \$PATH."
     exit 0
 else
     echo "Something went wrong, please send an email to contact@intoli.com with details about the bundle."
