390868ea07c28d35970924be6c5dcf418d33c987
Evan Sangaline <esangaline@gmail.com>
2018-03-03 12:25:20 -0500

    Add a method for extracting a filename from an exec call.

--- src/exodus_bundler/input_parsing.py
+++ src/exodus_bundler/input_parsing.py
@@ -1,3 +1,27 @@
+exec_methods = [
+    'execve',
+    'exec',
+    'execl',
+    'execlp',
+    'execle',
+    'execv',
+    'execvp',
+    'execvpe',
+]
+
+
+def extract_exec_filename(line):
+    """Parse a line of strace output and returns the file being executed."""
+    for method in exec_methods:
+        prefix = method + '("'
+        if line.startswith(prefix):
+            line = line[len(prefix):]
+            parts = line.split('", ')
+            if len(parts) > 1:
+                return parts[0]
+    return None
+
+
 def extract_filenames(content):
     """Parses filenames from a piped input.
 
