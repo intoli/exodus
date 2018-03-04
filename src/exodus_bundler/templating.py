cf6a7a55bab63e90de0737a5156312c124144465
Evan Sangaline <esangaline@gmail.com>
2018-03-04 15:17:26 -0500

    Fix the bbfreeze template hack.

--- src/exodus_bundler/templating.py
+++ src/exodus_bundler/templating.py
@@ -8,9 +8,11 @@ import os
 parent_directory = os.path.dirname(os.path.realpath(__file__))
 template_directory = os.path.join(parent_directory, 'templates')
 
+
 # This is kind of a hack to make the templates accessible using bbfreeze.
-if os.path.dirname(parent_directory) == 'library.zip' and not os.path.exists(template_directory):
-    template_directory = os.normpath(os.path.join(parent_directory, '..', '..', 'templates'))
+grandparent_basename = os.path.basename(os.path.dirname(parent_directory))
+if grandparent_basename == 'library.zip' and not os.path.exists(template_directory):
+    template_directory = os.path.normpath(os.path.join(parent_directory, '..', '..', 'templates'))
 
 
 def render_template(string, **context):
