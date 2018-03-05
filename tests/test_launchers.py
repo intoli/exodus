8342c560b11de3ed0bb1aea0d7a869467384ae13
Evan Sangaline <esangaline@gmail.com>
2018-03-05 18:03:05 -0500

    Add a test for `find_executable()`.

--- tests/test_launchers.py
+++ tests/test_launchers.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import stat
 import tempfile
 from subprocess import PIPE
@@ -6,13 +7,18 @@ from subprocess import Popen
 
 import pytest
 
+from exodus_bundler import launchers
+from exodus_bundler.bundling import create_unpackaged_bundle
 from exodus_bundler.launchers import CompilerNotFoundError
 from exodus_bundler.launchers import compile_diet
 from exodus_bundler.launchers import compile_musl
 from exodus_bundler.launchers import construct_bash_launcher
+from exodus_bundler.launchers import find_executable
 
 
 parent_directory = os.path.dirname(os.path.realpath(__file__))
+chroot = os.path.join(parent_directory, 'data', 'binaries', 'chroot')
+echo_args_glibc_32 = os.path.join(chroot, 'bin', 'echo-args-glibc-32')
 fizz_buzz_source_file = os.path.join(parent_directory, 'data', 'binaries', 'fizz-buzz.c')
 
 
@@ -49,3 +55,25 @@ def test_compile(compiler):
     stdout, stderr = process.communicate()
     assert 'FIZZBUZZ' in stdout.decode('utf-8')
     assert len(stderr.decode('utf-8')) == 0
+
+
+def test_find_executable():
+    original_environment = os.environ.get('PATH')
+    original_parent_directory = launchers.parent_directory
+
+    root_directory = create_unpackaged_bundle(
+        rename=[], executables=[echo_args_glibc_32], chroot=chroot)
+    try:
+        binary_name = os.path.basename(echo_args_glibc_32)
+        binary_symlink = os.path.join(root_directory, 'bin', binary_name)
+        binary_path = os.path.realpath(binary_symlink)
+        # This is a pretend directory, but it doesn't check.
+        launchers.parent_directory = os.path.join(os.path.dirname(binary_path), 'somewhere', 'else')
+        os.environ['PATH'] = os.path.dirname(echo_args_glibc_32)
+        assert find_executable(binary_name, skip_original_for_testing=True) == binary_path, \
+            'It should have found the binary path "%s".' % binary_path
+    finally:
+        launchers.parent_directory = original_parent_directory
+        os.environ['PATH'] = original_environment
+        assert root_directory.startswith('/tmp/')
+        shutil.rmtree(root_directory)
