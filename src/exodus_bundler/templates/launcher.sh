da038c04d8bb66f5e6b4a827a6adcd8b74451e61
Evan Sangaline <esangaline@gmail.com>
2018-03-05 17:18:52 -0500

    Update the shell launchers to support both limited and full linkers.

--- src/exodus_bundler/templates/launcher.sh
+++ src/exodus_bundler/templates/launcher.sh
@@ -5,4 +5,8 @@ executable="${current_directory}/{{executable}}"
 library_path="{{library_path}}"
 library_path="${current_directory}/${library_path//:/:${current_directory}/}"
 linker="${current_directory}/{{linker_dirname}}/{{linker_basename}}"
-exec "${linker}" --library-path "${library_path}" --inhibit-rpath "" "${executable}" "$@"
+if [ "{{full_linker}}" == "true" ]; then
+    exec "${linker}" --library-path "${library_path}" --inhibit-rpath "" "${executable}" "$@"
+else
+    exec "${linker}" --library-path "${library_path}" "${executable}" "$@"
+fi
