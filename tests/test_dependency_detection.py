ef703c73ffd76d108da31c5d4a3d6e80e5bbc88e
Evan Sangaline <esangaline@gmail.com>
2018-03-05 21:18:28 -0500

    Check both `/bin/` and `/usr/bin/` for the `ls` executable.

--- tests/test_dependency_detection.py
+++ tests/test_dependency_detection.py
@@ -2,7 +2,13 @@ from exodus_bundler.dependency_detection import detect_dependencies
 
 
 def test_detect_dependencies():
+    # This is a little janky, but the test suite won't run anywhere where it's not true.
+    ls = '/usr/bin/ls'
+    if not os.path.exists(ls):
+        ls = '/bin/ls'
+    assert os.path.exists(ls), 'This test assumes that `ls` is installed on the system.'`
+
     dependencies = detect_dependencies('/usr/bin/ls')
     print(dependencies)
-    assert any('/usr/bin/ls' in dependency for dependency in dependencies), \
-        '`/usr/bin/ls` should have been detected as a dependency for `ls`.'
+    assert any(ls in dependency for dependency in dependencies), \
+        '`%s` should have been detected as a dependency for `ls`.' % ls
