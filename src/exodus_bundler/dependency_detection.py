ff01982c2788a4122bf6a0343e31010498abdae4
Evan Sangaline <esangaline@gmail.com>
2018-03-05 21:04:41 -0500

    Add detection of debian dependencies.

--- src/exodus_bundler/dependency_detection.py
+++ src/exodus_bundler/dependency_detection.py
@@ -10,6 +10,10 @@ def detect_dependencies(path):
     if dependencies:
         return dependencies
 
+    dependencies = detect_debian_dependencies(path)
+    if dependencies:
+        return dependencies
+
     return None
 
 
@@ -37,6 +41,37 @@ def detect_arch_dependencies(path):
         if not line.startswith(prefix):
             continue
         dependency_path = line[len(prefix):]
+        if os.path.exists(dependency_path):
+            dependencies.append(dependency_path)
+
+    return dependencies
+
+
+def detect_debian_dependencies(path):
+    cache_directory = '/var/cache/apt'
+    if not (os.path.exists(cache_directory) and os.path.isdir(cache_directory)):
+        return None
+
+    dpkg = resolve_binary('dpkg')
+    if not dpkg:
+        return None
+
+    process = subprocess.Popen([dpkg, '-S', path], stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    parts = stdout.decode('utf-8').split(': ')
+    if len(parts) != 2:
+        return None
+
+    package_name = parts[0]
+    dpkg_query = resolve_binary('dpkg-query')
+    if not dpkg_query:
+        return None
+
+    package_name = parts[0]
+    process = subprocess.Popen([dpkg_query, '-L', package_name], stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    dependencies = []
+    for dependency_path in stdout.decode('utf-8').split('\n'):
         if os.path.exists(dependency_path):
             dependencies.append(dependency_path)
 
