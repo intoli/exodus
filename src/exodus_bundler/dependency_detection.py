78cbb1a381a6948e95457579760ba22470109843
Evan Sangaline <esangaline@gmail.com>
2018-03-07 15:22:06 -0500

    Create a `Pacman` class and use it in `detect_arch_dependencies()`.

--- src/exodus_bundler/dependency_detection.py
+++ src/exodus_bundler/dependency_detection.py
@@ -69,6 +69,14 @@ class PackageManager(object):
         return all(find_executable(command) is not None for command in commands)
 
 
+class Pacman(PackageManager):
+    cache_directory = '/var/cache/pacman'
+    list_command = ['pacman', '-Ql']
+    list_regex = '.*\s+(\/.+)'
+    owner_command = ['pacman', '-Qo']
+    owner_regex = ' is owned by (.*)\s+.*'
+
+
 def detect_dependencies(path):
     # We'll go through the supported systems one by one.
     dependencies = detect_arch_dependencies(path)
@@ -87,33 +95,8 @@ def detect_dependencies(path):
 
 
 def detect_arch_dependencies(path):
-    cache_directory = '/var/cache/pacman'
-    if not (os.path.exists(cache_directory) and os.path.isdir(cache_directory)):
-        return None
-
-    pacman = find_executable('pacman')
-    if not pacman:
-        return None
-
-    process = subprocess.Popen([pacman, '-Qo', path], stdout=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    parts = stdout.decode('utf-8').split(' is owned by ')
-    if len(parts) != 2:
-        return None
-
-    package_name = parts[1].split(' ')[0]
-    process = subprocess.Popen([pacman, '-Ql', package_name], stdout=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    dependencies = []
-    for line in stdout.decode('utf-8').split('\n'):
-        prefix = '%s ' % package_name
-        if not line.startswith(prefix):
-            continue
-        dependency_path = line[len(prefix):]
-        if os.path.exists(dependency_path) and not os.path.isdir(dependency_path):
-            dependencies.append(dependency_path)
-
-    return dependencies
+    pacman = Pacman()
+    return pacman.find_dependencies(path)
 
 
 def detect_debian_dependencies(path):
