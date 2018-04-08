6b0355513fb29081f43a755cfa10bf5080058f65
Evan Sangaline <esangaline@gmail.com>
2018-04-08 10:56:54 -0400

    Bump version: 2.0.1 → 2.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name='exodus-bundler',
-    version='2.0.1',
+    version='2.0.2',
     license='BSD',
     platforms=['Linux'],
     description='The exodus application bundler.',
